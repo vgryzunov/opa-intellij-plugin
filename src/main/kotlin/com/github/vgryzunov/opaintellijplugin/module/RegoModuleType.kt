d8f16d7252ba12c89ae73dad36f14739687d56da
vgryzunov <vgryzunov@reinform.ru>
2023-01-27 19:16:40 +0300

    Moved module-related files to a distinct package

---
+++ src/main/kotlin/com/github/vgryzunov/opaintellijplugin/module/RegoModuleType.kt
@@ -0,0 +1,46 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package com.github.vgryzunov.opaintellijplugin.module
+
+import com.intellij.ide.util.projectWizard.ModuleWizardStep
+import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.module.ModuleTypeManager
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider
+import com.github.vgryzunov.opaintellijplugin.lang.RegoIcons
+import javax.swing.Icon
+
+class RegoModuleType : ModuleType<RegoModuleBuilder>("REGO_MODULE") {
+    override fun createModuleBuilder(): RegoModuleBuilder {
+        return RegoModuleBuilder()
+    }
+
+    override fun getName(): String {
+        return "Rego Project"
+    }
+
+    override fun getDescription(): String {
+        return "A Rego project is empty. Create a new Rego File to add."
+    }
+
+    override fun getNodeIcon(isOpened: Boolean): Icon {
+        return RegoIcons.OPA
+    }
+
+    override fun createWizardSteps(wizardContext: WizardContext, moduleBuilder: RegoModuleBuilder, modulesProvider: ModulesProvider): Array<ModuleWizardStep> {
+        return super.createWizardSteps(wizardContext, moduleBuilder, modulesProvider)
+    }
+
+    companion object {
+        private const val ID = "REGO_MODULE"
+        val INSTANCE: RegoModuleType by lazy { ModuleTypeManager.getInstance().findByID(
+            ID
+        ) as RegoModuleType
+        }
+    }
+
+
+}
